b5b88b1c24a109f609fd2515cff30af29a0381cd
delosantosronajoy <delosantosronajoy@gmail.com>
2024-06-08 12:03:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -42,4 +42,4 @@ const commentbox = document.getElementById("commentbox")
 function submitComment(){
     commentbox.textContent += comment.value.toString() + "\n"
 }
-submit.addEventListener("click",submitComment)
+submit.addEventListener("click", submitComment)
